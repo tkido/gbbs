0b56e7eca3b4b586ace19ff68eb5d9fd30312ab7
tkido <takanorikido@gmail.com>
2013-12-10 12:34:52 +0900

    link and anchor

--- server/tengine.py
+++ server/tengine.py
@@ -20,21 +20,24 @@ ANCHOR_PAT = re.compile('&gt;&gt;(1000|0|[1-9][0-9]{0,2})(-?)((1000|0|[1-9][0-9]
 GRUBER_URLINTEXT_PAT = re.compile(ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
  
 def decorate(source):
-    def replace_anchor(match):
-        g = match.groups()
-        if g[1]:
-            return '<a href=\"#%s\">&gt;&gt;</a><a href=\"%s%s%s\">%s%s%s</a>' % \
-                   (g[0], g[0], g[1], g[2], g[0], g[1], g[2])
-        elif g[2] and not g[1]:
-            return match.group(0)
-        else:
-            return '<a href=\"#%s\">&gt;&gt;</a><a href=\"%s\">%s</a>%s%s' % \
-                   (g[0], g[0], g[0], g[1], g[2])
-    def replace_uri(match):
-        g = match.group()
-        return '<a href=\"%s\">%s</a>' % (g, g)
-    source = re.sub(GRUBER_URLINTEXT_PAT, replace_uri, source)
-    return re.sub(ANCHOR_PAT, replace_anchor, source)
+    def anchor(source):
+        def replaced(match):
+            g = match.groups()
+            if g[1]:
+                return '<a href=\"#%s\">&gt;&gt;</a><a href=\"%s%s%s\">%s%s%s</a>' % \
+                       (g[0], g[0], g[1], g[2], g[0], g[1], g[2])
+            elif g[2] and not g[1]:
+                return match.group(0)
+            else:
+                return '<a href=\"#%s\">&gt;&gt;</a><a href=\"%s\">%s</a>%s%s' % \
+                       (g[0], g[0], g[0], g[1], g[2])
+        return re.sub(ANCHOR_PAT, replaced, source)
+    def link(match):
+        def replaced(match):
+            g = match.group()
+            return '<a href=\"%s\">%s</a>' % (g, g)
+        return re.sub(GRUBER_URLINTEXT_PAT, replaced, source)
+    return anchor(link(source))
 
 def render(tempalte, context, **kwargs):
     return tengine.render(tempalte, context, **kwargs)
